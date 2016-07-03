84294c317227ef344bce650cde05cc691dea8bb3
pedronote <pedr0_r@hotmail.com>
2016-07-03 20:30:50 -0300

    2060 solved

--- 2060.c
+++ 2060.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
 int main() {
+	int numTests;
+	scanf("%d", &numTests);
 
+	int i;
+	int numbers[numTests];
+	int multiple2 = 0;
+	int multiple3 = 0;
+	int multiple4 = 0;
+	int multiple5 = 0;
+
+	for (i = 0; i < numTests; i++) {
+		scanf("%d", &numbers[i]);
+		if (numbers[i] % 2 == 0)
+			multiple2++;
+		if (numbers[i] % 3 == 0)
+			multiple3++;
+		if (numbers[i] % 4 == 0)
+			multiple4++;
+		if (numbers[i] % 5 == 0)
+			multiple5++;
+	}
+
+	printf("%d Multiplo(s) de 2\n", multiple2);
+	printf("%d Multiplo(s) de 3\n", multiple3);
+	printf("%d Multiplo(s) de 4\n", multiple4);
+	printf("%d Multiplo(s) de 5\n", multiple5);
 }
--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - [1068 - Solved](https://www.urionlinejudge.com.br/judge/en/problems/view/1068)
 - [1564 - Solved](https://www.urionlinejudge.com.br/judge/en/problems/view/1564)
 - [1072 - Solved](https://www.urionlinejudge.com.br/judge/en/problems/view/1072)
+- [1001 - Solved](https://www.urionlinejudge.com.br/judge/en/problems/view/1001)
+- [1002 - Solved](https://www.urionlinejudge.com.br/judge/en/problems/view/1002)
+- [1035 - Solved](https://www.urionlinejudge.com.br/judge/en/problems/view/1035)
+- [2060 - Solved](https://www.urionlinejudge.com.br/judge/en/problems/view/2060)

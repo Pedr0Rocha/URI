19e1e048cd67830cd591c15918d753ba3ae621ce
pedronote <pedr0_r@hotmail.com>
2016-07-03 20:20:03 -0300

    1001, 1002, 1035 solved

---
+++ 1001.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int value1, 
+		value2;
+
+	scanf("%d", &value1);
+	scanf("%d", &value2);
+	printf("X = %d\n", (value1 + value2));
+}
---
+++ 1002.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define N 3.14159
+
+int main() {
+	double raio;
+	scanf("%lf", &raio);
+
+	printf("A=%.4lf\n", (N * (raio * raio)));
+}

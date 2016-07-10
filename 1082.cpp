fb979cd7e40a8cd43e9cc183a5433f7397965c53
Pedro Rocha <pedr0_r@hotmail.com>
2016-07-10 15:17:34 -0300

    1082 solved

--- 1082.cpp
+++ 1082.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 using namespace std;
 
+#define CHARCONVERTION 97
 #define WHITE 0
 #define GRAY 1
 #define BLACK 2
@@ -10,60 +11,62 @@ using namespace std;
 
 typedef vector<int> vi;
 
-vector<vi> Adjlist;
-vector<int> color;
-int currentTime = 0;
-int numVertices, numEdges;
+vector<vi> adjList;
+vector<int> colors;
+vector<int> components;
 
-void dfsVisit(int vertex) {
-	currentTime++;
-	color[vertex] = GRAY;
-	for (int i = 0; i < Adjlist[vertex].size(); i++) {
-		int v = Adjlist[vertex][i];
+int cnt = 0,v,a,x,y;
 
-		dfsVisit(v);
-	}
-	color[vertex] = BLACK;
-}
-
-void dfs(int vertex) {
-	if (color[vertex] == WHITE && Adjlist[vertex].size() > 0)
-		dfsVisit(vertex);
-
-	for (int i = 1; i < numVertices; i++) {
-		currentTime = -1; 
-		if (color[i] == WHITE && Adjlist[i].size() > 0) {
-			if (Adjlist[0].size() > 0)
-				cout << endl;
-			dfsVisit(i);
+void dfs(int current) {
+	colors[current] = 1;
+	cnt += 2;
+	components.push_back(current);
+	for(int i = 0; i < (int) adjList[current].size(); i++) {
+		int v = adjList[current][i];
+		if(colors[v] == 0) {
+			dfs(v);
 		}
 	}
-	cout << endl;
+	colors[current] = 2;
 }
 
-int main() {
+
+int main() {	
+	int casesIndex = 1;
 	int cases;
-	int caseIndex = 0;
 	cin >> cases;
-
-	while (cases-- > 0) {
-		caseIndex++;
-		cout << "Case #" << caseIndex << ":" << endl;
-		
-		cin >> numVertices >> numEdges;
-		
-		Adjlist.assign(numVertices, vector<int>());
-		color.assign(numVertices, 0);
-
-		for (int i = 0; i < numEdges; i++) {
-			char v, u;
-			cin >> v >> u;
-			int x = (int)a - 97;
-			int y = (int)a - 97;
-			Adjlist[v].push_back(u);
-			Adjlist[u].push_back(v);
+	for (int o = 0; o < cases; o++) {
+		cnt = 0;
+		cin >> v >> a;
+		int numComponents = 0;
+		adjList.assign(v,vector<int>());
+		colors.assign(v,0);
+		for(int i = 0; i < a; i++) {
+			char a,b;
+			cin >> a >> b;
+			int x = ((int)a) - CHARCONVERTION,
+				y = ((int)b) - CHARCONVERTION;
+			
+			adjList[x].push_back(y);
+			adjList[y].push_back(x);
 		}
-		//dfs(0);
+		for(int i = 0; i < v; i++)
+			sort(adjList[i].begin(), adjList[i].end());
+
+		cout << "Case #" << casesIndex++ << ":"<< endl ;
+		for(int i = 0; i < v; i++) {
+			if(!colors[i]) {
+				numComponents++;
+				dfs(i);
+				sort(components.begin(),components.end());
+				for (int u = 0; u < (int) components.size(); u++){
+					cout <<  (char)(components[u]+CHARCONVERTION) << ",";
+				}
+				components.clear();
+				cout << endl;
+			}
+			
+		}
+		cout << numComponents << " connected components" << endl << endl;
 	}
-	return 0;
 }
